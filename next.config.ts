b46d84a169bcff387eab7a0eae708faa1639ca5d
cmor03 <coltonhmorris@gmail.com>
2025-04-24 20:02:49 -0600

    checkpoint 1 - model and style

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'keepsake-three.vercel.app'],
   },
   // Add configuration to serve files from the uploads directory
   async rewrites() {
